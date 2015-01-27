7f9aec816847e368ba29cfefbe7c65b46505937d
alan@functionfirst.co.uk <alan@functionfirst.co.uk>
2015-01-27 00:03:33 +0000

    Updating to new design.

---
+++ BuildmateWebsite/obj/Release/Package/PackageTmp/Content/js/functions.js
@@ -0,0 +1,11 @@
+﻿jQuery("document").ready(function ($) {
+    var nav = $('body');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 136) {
+            nav.addClass("fixed-nav");
+        } else {
+            nav.removeClass("fixed-nav");
+        }
+    });
+});
